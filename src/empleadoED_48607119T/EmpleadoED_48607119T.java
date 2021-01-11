d7f1e47cb6005d5eccf33c27a8a42e78f3354b44
Bernat Pérez <perezroig.bernat@mail.com>
2021-01-11 17:08:27 +0100

    Versión final con JAVADOC DNI 48607119T

--- src/empleadoED_48607119T/EmpleadoED_48607119T.java
+++ src/empleadoED_48607119T/EmpleadoED_48607119T.java
@@ -1,6 +1,11 @@
 package empleadoED_48607119T;
 
 import java.util.*;
+/**
+ * Ejercicio práctico examen EVAL 1 ED
+ * @author BERNAT PÉREZ DNI 48607119T
+ * @version 1.0
+ */
 
 
 public class EmpleadoED_48607119T {
@@ -8,12 +13,24 @@ public class EmpleadoED_48607119T {
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
+    /**
+     * Constructor con tres parámetros
+     * 
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de identidad con letra y sin espacios
+     */
 
     public EmpleadoED_48607119T(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
-
+    /**
+    * Calcula el salario bruto mensual en función del salario base por hora y las horas extras
+    * El precio por hora de la hora extra se determina con la constante FACTOR_HORA_EXTRA
+    * 
+    * @param horasExtra número de horas extra dedicadas redondeado a la baja (entero)
+    * @return devuelve el salario bruto mensual en euros con dos decimales
+    */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
